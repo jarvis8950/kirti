dd9bcef573ac45dfd232042f68b028d36af2178e
jarvis8950 <39271795+jarvis8950@users.noreply.github.com>
2019-08-21 00:53:57 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+<div class="container">
+    <h1>Login here </h1>
+  <form class="" action="index.php" method="post">
+    <label for="">email</label>
+    <input  id="email" type="email" name="email" value=""
+    placeholder="email" required>
+
+    <label for="">password</label>
+    <input  id="password" type="password" name="password" value=""
+    placeholder="**********" required>
+    <button type="submit" name="login">submit</button>
+  </form>
+</div>
+
+
+  </body>
+</html>
